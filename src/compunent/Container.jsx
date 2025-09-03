ef6db11714e66be519e33aba418bcdab0c76da93
Md. Salim Hosen <salimhosen2023@gmail.com>
2025-09-03 17:02:47 +0600

    first commit

---
+++ src/compunent/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Container = ({children, className}) => {
+  return (
+    <div className={`max-w-[1200px] mx-auto ${className}`}>  {children} </div>
+  )
+}
+
+export default Container
---
+++ src/compunent/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Container>
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum, ut!</h1>
+      </Container>
+    </div>
+  )
+}
+
+export default Navbar
